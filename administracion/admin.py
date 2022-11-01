9ca0c8ad2e481ce603499fb9687653ca86883dc0
jA71 <isjavier77@gmail.com>
2022-11-01 10:35:50 -0500

    Se sube el proyecto de la Ruta del Cafe

---
+++ administracion/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Emprendedor, Emprendimiento, Cliente, Adminstrador
+
+# Register your models here.
+
+admin.site.register(Emprendedor)
+admin.site.register(Emprendimiento)
+admin.site.register(Cliente)
+admin.site.register(Adminstrador)
---
+++ administracion/models.py
@@ -0,0 +1,74 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class Emprendimiento(models.Model):
+
+    TIPO = [
+        ('hospedaje', 'Hospedaje'),
+        ('restaurantes', 'Restaurante'),
+        ('spots', 'Spot Turistico'),
+        ('productores', 'Productor - Vendedor'),
+    ]
+
+    tipo_emprendimiento = models.CharField(verbose_name="Tipo de Emprendimiento", max_length=200, choices=TIPO)
+    empresa = models.CharField(verbose_name="Nombre del Emprendimiento", max_length=200)
+    direccion = models.CharField(verbose_name="Dirección", max_length=200)
+    telefono = models.CharField(verbose_name="Teléfono", max_length=13)
+    decripcion = models.TextField()
+    horario_apertura = models.DateTimeField()
+    horario_cierre = models.DateTimeField()
+    altitud = models.CharField(verbose_name="Altitud", max_length=20)
+    latitud = models.CharField(verbose_name="Latitud", max_length=20)
+    foto = models.ImageField(upload_to = "fotos_local", verbose_name = "Logo", null = True, blank = True)
+
+
+class Producto(models.Model):
+    nombre_producto = models.CharField(verbose_name="Nombre del Producto", max_length=100)
+    descripcion_producto = models.TextField
+    cantidad = models.IntegerField(verbose_name="Cantidad del Prodcuto", max_length=4)
+    
+
+
+
+class Persona(models.Model):
+
+    TIPO_DOCUMENTO = [
+        ('cedula','Cedula'),
+        ('pasaporte','Pasaporte'),
+
+    ]
+
+    tipo_documento = models.CharField(verbose_name="Tipo de Documento", max_length=20, choices = TIPO_DOCUMENTO)
+    num_documento = models.CharField(verbose_name="Número de Documento", max_length = 13)
+    nombre = models.CharField(verbose_name="Nombre", max_length = 100)
+    apellido = models.CharField(verbose_name="Apellido", max_length = 100)
+    email = models.EmailField(verbose_name="E-mail")
+    telefono = models.CharField(verbose_name="Teléfono", max_length=13)
+    direccion = models.CharField(verbose_name="Dirección", max_length=13)
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de Nacimiento")
+    pais_origen = models.CharField(verbose_name="País de origen", max_length=200)
+    foto = models.ImageField(upload_to = "fotos_usuarios", verbose_name = "Foto de Perfil", null = True, blank = True)
+
+    # metodo para presentar el objeto creado
+    def __str__(self):
+        return self.cedula
+
+
+class Cliente(Persona):
+    pass
+
+
+class Adminstrador(Persona):
+    fecha_inicio = models.DateTimeField()
+    fecha_actualizacion = models.DateTimeField()
+    estado = models.BooleanField()
+
+
+class Emprendedor(Persona):
+    pass
+
+
+
+
